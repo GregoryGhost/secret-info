fc86b93d5345a09bea9dd6362fa5f53572460872
Gregory <lobster287@gmail.com>
2018-05-13 18:41:55 +0700

    Added base abstraction of domain model

---
+++ StegoModel/Worker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+
+namespace StegoModel
+{
+    public interface IWorker
+    {
+        Bitmap Pack(Bitmap sourceImage, string text);
+        string Unpack(Bitmap stegoImage);
+    }
+
+    public interface IHelperIO
+    {
+        Bitmap ReadImage(string path);
+        void WriteImage(string path, Bitmap image);
+    }
+}
