9b83fa932e5f7b177a300f7ff30e58eb38aee3ad
Gregory <lobster287@gmail.com>
2018-05-13 19:02:19 +0700

    Added helper methods for converting bites to byte and byte to bites

---
+++ StegoModel/Helpers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StegoModel
+{
+    public static class BitsExtension
+    {
+        public static BitArray ToBits(this byte src)
+        {
+            const int sizeByte = 8;
+            BitArray bitArray = new BitArray(sizeByte);
+            bool st = false;
+
+            for (int i = 0; i < 8; i++)
+            {
+                st = (src >> i & 1) == 1 ? true : false;
+                bitArray[i] = st;
+            }
+
+            return bitArray;
+        }
+
+        public static byte ToByte(this BitArray scr)
+        {
+            byte num = 0;
+
+            for (int i = 0; i < scr.Count; i++)
+            {
+                if (scr[i] == true)
+                    num += (byte)Math.Pow(2, i);
+            }
+
+            return num;
+        }
+    }
+}
