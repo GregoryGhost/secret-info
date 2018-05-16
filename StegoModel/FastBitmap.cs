0f825b5e74b3f84cf584e4e40eea855ed2ed7d5d
Gregory <lobster287@gmail.com>
2018-05-16 23:50:15 +0700

    Refactor: added xml-comments for members of fast bitmap class

--- StegoModel/FastBitmap.cs
+++ StegoModel/FastBitmap.cs
@@ -6,8 +6,14 @@ namespace StegoModel
 {
     namespace ImageFilter
     {
+        /// <summary>
+        /// Быстрая реализация работы с изображениями.
+        /// </summary>
         unsafe public class FastBitmap
         {
+            /// <summary>
+            /// Внутреннее представление пикселя.
+            /// </summary>
             private struct PixelData
             {
                 public byte blue;
@@ -15,6 +21,10 @@ namespace StegoModel
                 public byte red;
                 public byte alpha;
 
+                /// <summary>
+                /// Преобразование значений каналов RGB пикселя в строку.
+                /// </summary>
+                /// <returns></returns>
                 public override string ToString()
                 {
                     return String.Format("({0}, {1}, {2}, {3})",
@@ -22,16 +32,38 @@ namespace StegoModel
                 }
             }
 
+            /// <summary>
+            /// Текущее рабочее изображение.
+            /// </summary>
             private Bitmap workingBitmap = null;
+
+            /// <summary>
+            /// Ширина рабочего изображения.
+            /// </summary>
             private int width = 0;
+
+            /// <summary>
+            /// Пиксели изображения.
+            /// </summary>
             private BitmapData bitmapData = null;
+
+            /// <summary>
+            /// Начальный пиксель изображения.
+            /// </summary>
             private Byte* pBase = null;
 
+            /// <summary>
+            /// Инициализация экземпляра.
+            /// </summary>
+            /// <param name="inputBitmap">Исходное изображение.</param>
             public FastBitmap(Bitmap inputBitmap)
             {
                 workingBitmap = inputBitmap;
             }
 
+            /// <summary>
+            /// Заблокировать доступ к изображению.
+            /// </summary>
             public void LockImage()
             {
                 var bounds = new Rectangle(Point.Empty, workingBitmap.Size);
@@ -45,8 +77,17 @@ namespace StegoModel
                 pBase = (Byte*)bitmapData.Scan0.ToPointer();
             }
 
+            /// <summary>
+            /// Указатель на текущий пиксель изображения.
+            /// </summary>
             private PixelData* pixelData = null;
 
+            /// <summary>
+            /// Получить цвет пикселя в точке (x, y).
+            /// </summary>
+            /// <param name="x">Координата пикселя по оси OX.</param>
+            /// <param name="y">Координата пикселя по оси OY.</param>
+            /// <returns>Возвращает цвет пикселя в точке (x, y).</returns>
             public Color GetPixel(int x, int y)
             {
                 pixelData = (PixelData*)(pBase + y * width 
@@ -55,6 +96,10 @@ namespace StegoModel
                     pixelData->green, pixelData->blue);
             }
 
+            /// <summary>
+            /// Получить следующий пиксель изображения.
+            /// </summary>
+            /// <returns>Возвращает следующий пиксель изображения.</returns>
             public Color GetPixelNext()
             {
                 pixelData++;
@@ -62,6 +107,12 @@ namespace StegoModel
                     pixelData->green, pixelData->blue);
             }
 
+            /// <summary>
+            /// Устанавливает цвет для пикселя в точке (x, y).
+            /// </summary>
+            /// <param name="x">Координата по OX пикселя.</param>
+            /// <param name="y">Координата по OY пикселя.</param>
+            /// <param name="color">Новый цвет пикселя.</param>
             public void SetPixel(int x, int y, Color color)
             {
                 PixelData* data = (PixelData*)(pBase + y * width
@@ -72,6 +123,9 @@ namespace StegoModel
                 data->blue = color.B;
             }
 
+            /// <summary>
+            /// Разблокировать доступ к изображению.
+            /// </summary>
             public void UnlockImage()
             {
                 workingBitmap.UnlockBits(bitmapData);
