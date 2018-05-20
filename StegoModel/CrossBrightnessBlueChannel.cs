2da263887630dca50cd33ca8f39345a78e14a5aa
Gregory <lobster287@gmail.com>
2018-05-21 04:16:52 +0700

    Refactor: added xml-comments for members of kdb(method cross) stego algo

--- StegoModel/CrossBrightnessBlueChannel.cs
+++ StegoModel/CrossBrightnessBlueChannel.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace StegoModel
 {
@@ -13,14 +12,25 @@ namespace StegoModel
     /// </summary>
     public class CrossBrightnessBlueChannel : IPacker, IUnpacker
     {
+        /// <summary>
+        /// Коэффициент, задающий энергию встраиваемого бита данных.
+        /// </summary>
         private readonly double _lambda = 0.5;
 
+        /// /// <summary>
+        /// Размер области(где находиться "крест"), 
+        ///     по которой будет прогнозироваться яркость.
+        /// </summary>
         private readonly int _sigma = 3;
 
+        /// <summary>
+        /// Помощник по работе с RGB-каналами стегоконтейнера.
+        /// </summary>
         private ExtractorRGB _rgbExtractor;
 
-        private const int _codepage = 1251;
-
+        /// <summary>
+        /// Инициализация необходимых для работы параметров.
+        /// </summary>
         public CrossBrightnessBlueChannel()
         {
             _rgbExtractor = new ExtractorRGB();
@@ -37,15 +47,12 @@ namespace StegoModel
         ///     который содержит скрытый текст.</returns>
         /// <exception cref="ArgumentException">
         /// Возникает, когда размер скрываемого текста 
-        ///     больше размера пустого контейнера,
-        ///     а также, когда пустой контейнер
-        ///     уже является стегоконтейнером.</exception>
+        ///     больше размера пустого контейнера</exception>
         public Bitmap Pack(Bitmap sourceImage, List<byte> text)
         {
             var msg = Encoding.Default.GetString(text.ToArray());
-            //var packedImg = sourceImage.Clone()
-            //    as Bitmap;
-            var packedImg = sourceImage;
+            var packedImg = sourceImage.Clone()
+                as Bitmap;
 
             packedImg = HideMessage(msg, packedImg);
 
@@ -65,7 +72,7 @@ namespace StegoModel
         ///     больше размера пустого контейнера.</exception>
         public List<byte> Unpack(Bitmap stegoImage)
         {
-            var container = stegoImage;//stegoImage.Clone() as Bitmap;
+            var container = stegoImage.Clone() as Bitmap;
             var msg = ExtractMessage(container);
             var unpackMsg = Encoding.Default
                 .GetBytes(msg).ToList();
@@ -73,6 +80,12 @@ namespace StegoModel
             return unpackMsg;
         }
 
+        /// <summary>
+        /// Скрыть сообщение в стегоконтейнере.
+        /// </summary>
+        /// <param name="text"><Тескст сообщения./param>
+        /// <param name="image">Изображение-стегоконтейнер.</param>
+        /// <returns>Возвращает стегоконтейнер со скрытым текстом.</returns>
         private Bitmap HideMessage(string text, Bitmap image)
         {
             var w = image.Width;
@@ -112,10 +125,13 @@ namespace StegoModel
                 {
                     for (int t = 0; t < chars.Length; t++)
                     {
+                        //высчитывание изменение яркости синего канала
+                        //  при встраивании информации
                         int br = Convert.ToInt32(
                             (0.299 * r[i, j] + 0.587 * g[i, j]
                                 + 0.114 * b[i, j]) * _lambda);
                         var ch = chars[t];
+                        //определение знака формулы для пикселя
                         int sing = (2 * Convert.ToInt32(
                             Convert.ToString(ch)) - 1);
 
@@ -145,9 +161,14 @@ namespace StegoModel
             return image;
         }
 
+        /// <summary>
+        /// Извлечь сообщение из стегоконтейнера.
+        /// </summary>
+        /// <param name="image">Изображение-стегоконтейнер.</param>
+        /// <returns>Возвращает текст сообщения, спрятанный
+        ///     в стегоконтейнере.</returns>
         public string ExtractMessage(Bitmap image)
         {
-
             _rgbExtractor.ExtractRGB(image);
             var r = _rgbExtractor.GetR();
             var g = _rgbExtractor.GetG();
@@ -161,7 +182,7 @@ namespace StegoModel
             for (int j = _sigma; j < h - h % 7; j += 7)
             {
                 for (int i = _sigma; i < w - w % 7; i += 7)
-                { //распаковка при _sigma = 3
+                { //распаковка квадранта "креста" при _sigma = 3
                     bStar[i, j] = (b[i - 1, j] + b[i - 2, j]
                         + b[i - 3, j] + b[i, j - 1]
                         + b[i, j - 2] + b[i, j - 3]
@@ -176,16 +197,30 @@ namespace StegoModel
                 }
             }
 
-            //перевод в строку из битовой строки
+            var unpackMsg = BitArrayToMessage(text);
+
+            return unpackMsg;
+        }
+
+        /// <summary>
+        /// Переводит битовую строку в строку символов.
+        /// </summary>
+        /// <param name="bits">Битовая строка.</param>
+        /// <returns>Возвращает строку символов
+        ///     исходного текста.</returns>
+        private string BitArrayToMessage(string bits)
+        {
             var bt = new byte[1];
-            //var info = new string[text.Length / 8];
             var info = "";
-            for (int i = 0; i < text.Length / 8; i++)
+            var sByte = 8;
+            var size = bits.Length / sByte;
+
+            for (int i = 0; i < size; i++)
             {
                 var str = "";
-                for (int j = 0; j < 8; j++)
+                for (int j = 0; j < sByte; j++)
                 {
-                    str += Convert.ToString(text[i * 8 + j]);
+                    str += Convert.ToString(bits[i * sByte + j]);
                 }
                 bt[0] = Convert.ToByte(str, 2);
                 info += Encoding.Default.GetString(bt);
@@ -194,16 +229,22 @@ namespace StegoModel
             return info;
         }
 
+        /// <summary>
+        /// Переводит текст сообщения в битовую строку.
+        /// </summary>
+        /// <param name="msg">Исходный текст сообщения.</param>
+        /// <returns>Возвращает битовую строку.</returns>
         private string MessageToBitArray(string msg)
         {
             var bytes = Encoding.Default.GetBytes(msg);
             var length = msg.Length;
             var result = String.Empty;
+            var sByte = 8;
 
-            for(var i = 0; i < length; i++)
+            for (var i = 0; i < length; i++)
             {
                 var str = Convert.ToString(msg[i], 2);
-                while (str.Length < 8)
+                while (str.Length < sByte)
                 {
                     var str1 = "0" + str;
                     str = str1;
@@ -212,21 +253,6 @@ namespace StegoModel
             }
 
             return result;
-        }
-
-        private double[,] CalculateBrightness(int[,] r,
-            int[,] g, int[,] b, int width, int height)
-        {
-            var y = new double[width, height];
-            for (int i = 0; i < width; i++)
-            {
-                for (int j = 0; j < height; j++)
-                {
-                    y[i, j] = (int)(0.298 * r[i, j]
-                        + 0.586 * g[i, j] + 0.114 * b[i, j]);
-                }
-            }
-            return y;
         }
     }
 }
